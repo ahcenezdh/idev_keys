dde53046ea13b0c8627849e4f24de67000480cc2
Space V <40030799+ahcenezdh@users.noreply.github.com>
2023-07-22 14:03:36 +0200

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -4,7 +4,7 @@ IDEV.Keys = {
     -- Maximum distance within which the player can interact with the vehicle keys
     MaxDistance = 4.0,
 
-    ControlKey = 'x', -- Key to use the keys on vehicle, NOTE: if you change this, you need to clear your fivem cache (client cache) since controls are cached into FiveM
+    ControlKey = 'x', -- Key to use the keys on vehicle, NOTE: if you change this (only if you're already joined your server with the script started), you need to clear your fivem cache (client cache) since controls are cached into FiveM
 
     -- Whether to enable key animation and key prop when using the key outside the vehicle
     EnableKeyAnimationOutside = true,
